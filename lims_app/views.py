696d747f396494ec8ee493d109dff643e047c155
Nanda Lal Das <tikludas01@gmail.com>
2024-02-13 00:19:53 +0530

    making the readerspage (in progress)

--- lims_app/views.py
+++ lims_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request,"home.html",context={'current_tab':'home'})
@@ -21,3 +21,10 @@ def save_student(request):
     student_name = request.POST['student_name']
     return render(request,'welcome.html',context={'student_name':student_name})
 
+def readers_tab(request):
+    readers = reader.objects.all()
+    return render(request,"readers.html",
+    context={
+        'current_tab':'readers',
+        'readers': readers,
+    })
