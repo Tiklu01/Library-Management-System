a1a1b33e8e0f38cd61acd4dbabe55c4d2560fa77
Nanda Lal Das <tikludas01@gmail.com>
2024-02-12 11:30:44 +0530

    fixed the CSRF verification failed error

--- lims_app/urls.py
+++ lims_app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns =[
-    path('',include('lims_app.urls')),
-    path('admin/', admin.site.urls),
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('home',home),
+    path('shop',shopping),
+    path('save',save_student)
 ]
--- lims_app/views.py
+++ lims_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    return render(request,"index.html",context={})
+
+def shopping(request):
+    return HttpResponse('Welcome to shopping')
+
+def save_student(request):
+    student_name = request.POST['student_name']
+    return HttpResponse('welcome to library - '+student_name)
